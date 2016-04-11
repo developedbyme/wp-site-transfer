d4ffa15f046efe1ede49ea15fc33121314641071
Mattias Ekendahl <mattias@developedbyme.com>
2016-04-11 17:20:26 +0200

    Update: added insertion of post

--- libs/OddSiteTransfer/Admin/TransferHooks.php
+++ libs/OddSiteTransfer/Admin/TransferHooks.php
@@ -42,49 +42,55 @@
 				'caller_get_posts'=> 1
 			);
 			
+			$post_type = $post->post_type;
+			//echo($post_type);
+			
 			$server_transfers_query = new WP_Query($args);
 			$server_transfers = $server_transfers_query->get_posts();
 			foreach($server_transfers as $server_transfer) {
 				$server_transfer_post_id = $server_transfer->ID;
 				
-				$base_url = get_post_meta($server_transfer_post_id, 'url', true);
-				echo($base_url);
+				if($post_type === 'post') { //METODO: make settings for this
+					//echo("++++");
+					$base_url = get_post_meta($server_transfer_post_id, 'url', true);
+					//echo($base_url);
 				
-				$post_ids = array(
-					'ID' => $post->ID
-				);
+					$post_ids = array(
+						'origin_id' => $post->ID,
+						'local_id' => NULL
+					);
 				
-				$post_data = array(
-					'post_title' => $post->post_title,
-					'post_content' => $post->post_content
-				);
+					$post_data = array(
+						'post_type' => $post->post_type,
+						'post_status' => $post->post_status,
+						'post_title' => $post->post_title,
+						'post_content' => $post->post_content
+					);
 				
-				//METODO
-				$url = $base_url.'sync/post';
+					//METODO
+					$url = $base_url.'sync/post';
 				
-				$fields_string = http_build_query(array('ids' => $post_ids, 'data' => $post_data));
+					$fields_string = http_build_query(array('ids' => $post_ids, 'data' => $post_data));
+					
+					$ch = curl_init();
+					curl_setopt($ch, CURLOPT_URL, $url);
+					curl_setopt($ch, CURLOPT_POST, TRUE);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+					//curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+					curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+					$data = curl_exec($ch);
+					$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+					curl_close($ch);
 				
-				$ch = curl_init();
-				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_POST, TRUE);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-				//curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-				curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-				$data = curl_exec($ch);
-				$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-				curl_close($ch);
-				
-				echo($httpcode);
-				echo($data);
-				
+					echo($httpcode);
+					echo($data);
+					
+					die; //MEDEBUG
+				}
 			}
 			wp_reset_query();
-			
-			
-			
-			die; //MEDEBUG
 		}
 		
 		public function hook_created_term($term_id, $tt_id, $taxonomy) {
--- libs/OddSiteTransfer/RestApi/SyncPostEndPoint.php
+++ libs/OddSiteTransfer/RestApi/SyncPostEndPoint.php
@@ -16,9 +16,28 @@
 		public function perform_call($data) {
 			//echo("\OddSiteTransfer\RestApi\SyncPostEndPoint::perform_call<br />");
 			
-			$data = $data["data"];
+			$post_ids = $data['ids'];
+			$data = $data['data'];
 			
-			return $this->output_success($data);
+			$local_id = $post_ids['local_id'];
+			
+			$new_id = NULL;
+			
+			if(!$local_id) {
+				
+				remove_all_actions('save_post'); //MEDEBUG
+				
+				$new_id = wp_insert_post($data);
+			}
+			else {
+				
+			}
+			
+			if($new_id) {
+				return $this->output_success($new_id);
+			}
+			return $this->output_error('Could not create/update post.');
+			
 		}
 		
 		public static function test_import() {
