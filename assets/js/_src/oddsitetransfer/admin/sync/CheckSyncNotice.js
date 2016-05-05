5063a65f37da5c440973ee413114b6e31d98ee48
Mattias Ekendahl <mattias@developedbyme.com>
2016-05-05 19:06:25 +0200

    Update: added option to resync a post

--- assets/js/_src/oddsitetransfer/admin/sync/CheckSyncNotice.js
+++ assets/js/_src/oddsitetransfer/admin/sync/CheckSyncNotice.js
@@ -13,6 +13,7 @@ export default class CheckSyncNotice extends React.Component {
 			"status": 0
 		};
 		
+		this.resyncBound = this.resync.bind(this);
 	}
 	
 	componentDidMount() {
@@ -29,16 +30,31 @@ export default class CheckSyncNotice extends React.Component {
 		}).bind(this));
 	}
 	
+	resync() {
+		this.setState({"status": 0});
+		jQuery.get(this.props.transferUrl + "?force=1", (function(aData) {
+			console.log(aData);
+			if(aData.code === "success") {
+				this.setState({"status": 1});
+			}
+			else {
+				this.setState({"status": -1});
+			}
+		}).bind(this)).fail((function() {
+			this.setState({"status": -1});
+		}).bind(this));
+	}
+	
 	render() {
 		
 		if(this.state.status === 1) {
 			return <p>
-				Post has been updated on all sites.
+			Post has been updated on all sites. <span className="resync" onClick={this.resyncBound}>(Resync)</span>
 			</p>;
 		}
 		if(this.state.status === -1) {
 			return <p>
-				An error occured while transferring.
+				An error occured while transferring. <span className="resync" onClick={this.resyncBound}>(Resync)</span>
 			</p>;
 		}
 		return <p>
