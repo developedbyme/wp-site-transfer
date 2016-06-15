b3103c812ef28d45d6bc0d829c9aed67523b9e4c
Mattias Ekendahl <mattias@developedbyme.com>
2016-06-15 21:30:58 +0200

    Update: checking if fiel exists for attachments

--- libs/OddSiteTransfer/RestApi/TransferWithDependency/SyncPostEndPoint.php
+++ libs/OddSiteTransfer/RestApi/TransferWithDependency/SyncPostEndPoint.php
@@ -155,6 +155,17 @@
 				case "post":
 					$post = $this->get_post_by_transfer_id($dependency_data['post_type'], $id);
 					if($post) {
+						if($dependency_data['post_type'] === 'attachment') {
+							var_dump($post);
+							
+							$file_name = get_post_meta($post->ID, '_wp_attached_file', true);
+							$base_file = wp_upload_dir()['basedir'].'/'.$file_name;
+							
+							if($file_name === '' || !file_exists($base_file)) {
+								$missing_dependencies[] = $dependency_data;
+							}
+						}
+						
 						$return_array[$type.'_'.$id] = $post;
 					}
 					else {
