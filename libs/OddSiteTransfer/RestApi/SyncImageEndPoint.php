256fb99a96a4e437a9c9480621cd80d538824d8e
Mattias Ekendahl <mattias@developedbyme.com>
2016-09-22 14:50:18 +0200

    Update: better error checking for upload

--- libs/OddSiteTransfer/RestApi/SyncImageEndPoint.php
+++ libs/OddSiteTransfer/RestApi/SyncImageEndPoint.php
@@ -18,6 +18,20 @@
 			
 			ini_set('memory_limit', '512M');
 			
+			switch( $_FILES['file']['error'] ) {
+				case UPLOAD_ERR_OK:
+					break;
+				case UPLOAD_ERR_INI_SIZE:
+				case UPLOAD_ERR_FORM_SIZE:
+					return $this->output_error("File size is too big (max size: ".get_max_upload().").");
+				case UPLOAD_ERR_PARTIAL:
+					return $this->output_error("File upload was partial.");
+				case UPLOAD_ERR_NO_FILE:
+					return $this->output_error("Zero length file was uploaded.");
+				default:
+					return $this->output_error("Unknown upload error (code: ".$_FILES['file']['error'].").");
+			}
+			
 			$path = $data['path'];
 			
 			$upload_dir = wp_upload_dir();
@@ -33,7 +47,6 @@
 			if(move_uploaded_file($_FILES['file']['tmp_name'], $file_to_save)) {
 				return $this->output_success($upload_dir['baseurl'].'/'.$path);
 			}
-			var_dump($_FILES);
 			return $this->output_error("Couldn't save file (".($_FILES['file']['tmp_name'])." to ".$file_to_save.").");
 		}
 		
