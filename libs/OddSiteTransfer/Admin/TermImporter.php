b882bac58b84c5a356d1f6d2aecb4c2c0ec7de73
Mattias Ekendahl <mattias@developedbyme.com>
2018-05-23 16:41:20 +0200

    Test: other debug data

--- libs/OddSiteTransfer/Admin/TermImporter.php
+++ libs/OddSiteTransfer/Admin/TermImporter.php
@@ -90,13 +90,12 @@
 			
 			if(isset($data['parent'])) {
 				$parent_term = $this->get_resolved_dependency('term', $data['parent'], $resolved_dependencies);
-				var_dump($parent_term);
 				if($parent_term) {
 					$term_data['parent'] = intval($parent_term->term_id);
 				}
 			}
 			
-			var_dump($data);
+			var_dump($term_data);
 			
 			if(!$existing_term) {
 				$result = wp_insert_term($term_data['name'], $taxonomy, $term_data);
