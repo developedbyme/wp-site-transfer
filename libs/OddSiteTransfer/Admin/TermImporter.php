d60f5bb1e9f82f4530e43fd04ec0ca873cb809a1
Mattias Ekendahl <mattias@developedbyme.com>
2018-08-03 10:17:59 +0200

    Update: hook for after import of term

--- libs/OddSiteTransfer/Admin/TermImporter.php
+++ libs/OddSiteTransfer/Admin/TermImporter.php
@@ -66,6 +66,8 @@
 				do_action(ODD_SITE_TRANSFER_DOMAIN.'/import_term/'.$transfer_type.'/meta/'.$category_name, intval($existing_term->term_id), $fields, $transfer_id);
 			}
 			
+			do_action(ODD_SITE_TRANSFER_DOMAIN.'/import_term/'.$transfer_type.'/after_import', intval($existing_term->term_id), $term_data, $data, $transfer_id);
+			
 			return intval($existing_term->term_id);
 		}
 		
